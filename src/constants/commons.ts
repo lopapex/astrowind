3b31f692473a3a909b90ed970270776a773a7287
Matěj Novotný <xnovot19@fi.muni.cz>
2025-01-26 18:08:02 +0100

    feat: add i18n support

---
+++ src/constants/commons.ts
@@ -0,0 +1,8 @@
+export const SOCIAL_LINKS = [
+  { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/3c.creates/' },
+  {
+    ariaLabel: 'Linkedin',
+    icon: 'tabler:brand-linkedin',
+    href: 'https://www.linkedin.com/in/mat%C4%9Bj-novotn%C3%BD-web-dev/',
+  },
+];
