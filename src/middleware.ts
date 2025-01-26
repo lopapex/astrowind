e2a4ba5503cd48c721c1cde3229bde49e16a77a0
Matěj Novotný <xnovot19@fi.muni.cz>
2025-01-26 18:38:22 +0100

    fix: resolve url

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,15 @@ import i18next from 'i18next';
 export const onRequest: MiddlewareHandler = async ({ request, redirect }, next) => {
   const url = new URL(request.url);
 
-  const [, lang] = url.pathname.split('/');
+  const [, lang, ...rest] = url.pathname.split('/');
 
   if (!lang || !i18next.languages.includes(lang)) {
     const newPath = url.pathname.replace(/\/$/, '');
     return redirect(`/${i18next.language}${newPath}`, 302);
+  }
+
+  if (rest.length > 0) {
+    return redirect(`/${lang}`, 302);
   }
 
   return next();
