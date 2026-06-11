80493975a8653ac8cebf9395613097e81524cc6d
Matěj Novotný <xnovot19@fi.muni.cz>
2026-06-11 21:59:19 +0200

    feat: update video

--- src/components/react/VideoAcademy.tsx
+++ src/components/react/VideoAcademy.tsx
@@ -6,9 +6,11 @@ type VideoAcademyProps = {
   id?: string;
   noSupport: string;
   signin: string;
+  text1: string;
+  text2: string;
 };
 
-const VideoAcademy = ({ id, noSupport, signin }: VideoAcademyProps) => {
+const VideoAcademy = ({ id, noSupport, signin, text1, text2 }: VideoAcademyProps) => {
   const [isPortrait, setIsPortrait] = useState(false);
 
   useEffect(() => {
@@ -40,6 +42,21 @@ const VideoAcademy = ({ id, noSupport, signin }: VideoAcademyProps) => {
         <source src={bgVideo} type="video/mp4" />
         {noSupport}
       </video>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-orange-500">
+        <img src="/images/videoAcademy.png" alt="CCC-academy" width={600} height={600} />
+
+        <div className="text-5xl lg:text-6xl uppercase flex flex-col justify-center items-center mt-[50px] mb-[-150px]">
+          <div className="font-secondary">{text1}</div>
+          <div className="font-secondary">{text2}</div>
+        </div>
+      </div>
+      <div className="absolute bottom-[10px] lg:bottom-[40px] left-1/2 transform -translate-x-1/2 z-10">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <a className="btn-header text-nowrap text-2xl py-2" href={FORM} target="_blank">
+            {signin}
+          </a>
+        </div>
+      </div>
 
       <div className="absolute bottom-[10px] lg:bottom-[40px] left-1/2 transform -translate-x-1/2 z-10">
         <div className="flex flex-col justify-center items-center gap-2">
